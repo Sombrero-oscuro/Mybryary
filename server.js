8c76709c24f8646e77527274e72516c8c79cd8f1
Mario Mamdouh <mario_mamdouh2@hotmail.com>
2022-10-12 02:39:45 +0200

    Initil Author Routes

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const db = mongoose.connection
 
 app.set('view engine' , 'ejs')
@@ -13,6 +15,7 @@ app.set('views' , __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlparser: true
@@ -23,5 +26,6 @@ db.on('error' , error => console.error(error))
 db.once('open' , () => console.log('Connected'))
 
 app.use('/' , indexRouter)
+app.use('/authors' , authorRouter)
 
 app.listen(process.env.PORT ||3000)
